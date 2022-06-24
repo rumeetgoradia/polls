c7cbbe69e6a899800d33040c6e6350bb3873f697
Rumeet Goradia <rumeet.goradia@gmail.com>
2022-06-24 13:37:24 -0400

    Merge guest account with logged in account

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter } from "@/backend/router/context";
 import superjson from "superjson";
 import { pollsRouter } from "./polls";
+import { usersRouter } from "./users";
 
 export const appRouter = createRouter()
 	.transformer(superjson)
-	.merge("polls.", pollsRouter);
+	.merge("polls.", pollsRouter)
+	.merge("users.", usersRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
---
+++ src/backend/router/users.ts
@@ -0,0 +1,38 @@
+import { createRouter } from "@/backend/router/context";
+import { prisma } from "@/db/prisma";
+import { z } from "zod";
+
+export const usersRouter = createRouter().mutation("merge-guest-account", {
+	input: z.object({
+		guestId: z.string(),
+	}),
+	async resolve({ input: { guestId }, ctx: { token: userId } }) {
+		if (!guestId || !userId || guestId === userId) {
+			return;
+		}
+
+		await prisma.poll.updateMany({
+			where: {
+				userId: guestId,
+			},
+			data: {
+				userId,
+			},
+		});
+
+		await prisma.vote.updateMany({
+			where: {
+				userId: guestId,
+			},
+			data: {
+				userId,
+			},
+		});
+
+		return await prisma.user.delete({
+			where: {
+				id: guestId,
+			},
+		});
+	},
+});
