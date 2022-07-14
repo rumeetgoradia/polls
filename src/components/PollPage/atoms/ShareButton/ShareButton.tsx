d291a63b687cf63be7da2c87382e13be1773c513
Rumeet Goradia <rumeet.goradia@gmail.com>
2022-07-14 17:43:25 -0400

    FIx VERCEL_URL

--- src/components/PollPage/atoms/ShareButton/ShareButton.tsx
+++ src/components/PollPage/atoms/ShareButton/ShareButton.tsx
@@ -1,6 +1,5 @@
 import { CopiedToast } from "@/components/PollPage/atoms/CopiedToast";
 import { COPIED_TOAST_ID } from "@/components/PollPage/atoms/CopiedToast/CopiedToast";
-import { getBaseUrl } from "@/utils/url";
 import { Button, useToast } from "@chakra-ui/react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { BsShare } from "react-icons/bs";
@@ -14,7 +13,11 @@ const ShareButton: React.FC<ShareButtonProps> = ({ pollId, colorScheme }) => {
 
 	return (
 		<CopyToClipboard
-			text={`${getBaseUrl()}/polls/${pollId}`}
+			text={`${
+				process.env.VERCEL_URL
+					? `https://${process.env.VERCEL_URL}`
+					: "http://localhost:3000"
+			}/polls/${pollId}`}
 			onCopy={() => {
 				if (!toast.isActive(COPIED_TOAST_ID)) {
 					toast({
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 import { SessionContextProvider } from "@/context/session";
 import theme, { Fonts } from "@/styles/theme";
-import { getBaseUrl } from "@/utils/url";
 import { withTRPC } from "@trpc/next";
 import { SessionProvider } from "next-auth/react";
 import { DefaultSeo } from "next-seo";
@@ -30,6 +29,16 @@ const MyApp: AppType = ({
 		</SessionProvider>
 	);
 };
+
+function getBaseUrl() {
+	if (typeof window !== "undefined") {
+		return "";
+	}
+	if (process.browser) return ""; // Browser should use current path
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+	return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
 
 export default withTRPC<AppRouter>({
 	config({ ctx }) {
