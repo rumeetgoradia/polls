6a3b69ff177b0316373d4f1ab6eaab8e666b499e
Rumeet Goradia <rumeet.goradia@gmail.com>
2022-07-14 17:13:21 -0400

    Fix font file name

--- src/styles/theme/fonts.tsx
+++ src/styles/theme/fonts.tsx
@@ -4,7 +4,7 @@ const Fonts = () => (
 		styles={`
         @font-face {
             font-family: 'Inter';
-            src: url('/fonts/inter.woff2') format('woff2');
+            src: url('/fonts/Inter.woff2') format('woff2');
             font-weight: 100 900;
             font-display: optional;
             font-style: normal;
