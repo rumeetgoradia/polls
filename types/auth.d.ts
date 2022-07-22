e85bb635d960ace8aec2904f49e485397d4751f7
Rumeet Goradia <rumeet.goradia@gmail.com>
2022-07-22 11:10:56 -0400

    Enhance isGuest determination for auth

--- types/auth.d.ts
+++ types/auth.d.ts
@@ -1,12 +1,22 @@
 import "next-auth";
+import "next-auth/jwt";
 
 declare module "next-auth" {
 	interface User {
 		id: string;
+		isGuest?: boolean;
 	}
 	interface Session {
 		user?: User;
 		expires: string;
-		isGuest: boolean;
+		isGuest?: boolean;
+	}
+}
+
+declare module "next-auth/jwt" {
+	/** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+	interface JWT {
+		/** OpenID ID Token */
+		isGuest?: boolean;
 	}
 }
